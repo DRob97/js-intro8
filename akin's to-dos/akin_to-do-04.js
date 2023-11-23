0d6673d26d83c687e60ef4ac780043a1a524289a
DRob97 <dylanlrobertson@gmail.com>
2023-11-22 20:56:28 -0600

    "added a copy file to do work in"

---
+++ akin's to-dos/akin_to-do-04.js
@@ -0,0 +1,21 @@
+/*
+    Create a class calles as Animal
+    Create a constructor which takes 'name', 'age', 'color'
+    Create 2 functions eat(), run()
+
+    THEN
+    Create a class called as Cat, which inherits all the Animal class properties and methods
+    Create a constructor which takes 'name', 'age', 'color', and 'breed'
+    Create 1 function play()
+
+    Create a class called as Dog and inherit all the Animal class properties and methods
+    Create a constructor which takes 'name', 'age', 'color', and 'breed'
+    Create 1 function catch()
+
+    THEN
+    Create a class calles as Husky, and inherit all the Dog class properties and methods
+    Create a constructor which takes 'name', 'age', 'color', 'breed', 'shed'
+    Create 1 function protect
+*/
+
+// Try this all with prototypes as well
