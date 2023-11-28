676471c8ba68933a31d93caa9837b21e46ae7f95
DRob97 <dylanlrobertson@gmail.com>
2023-11-27 18:53:53 -0600

    added 3 new note files and a recap file for js-async

---
+++ js-async/07-js-async-recap.js
@@ -0,0 +1,81 @@
+/*
+Go to https://www.techglobalschool.com/
+Validate the main heading is "Looking to pursue a new career?"
+*/
+/*
+function goToURL(){
+    setTimeout(() => {
+        console.log('You are on TechGlobal Website');
+    }, 2000);
+}
+
+function validateMainHeading(){
+    const actualText = '';  // You will learn how to get this later
+    const expectedText = 'Looking to pursue a new career?'; // Given in requirements
+    if(expectedText === actualText) console.log('Validation is successful!');
+    else console.log('Validation failed');
+}
+
+function test1(){
+    goToURL();
+    validateMainHeading();
+}
+
+test1();
+*/
+/*
+Expected output:
+    'You are on TechGlobal Website'
+    'Validation failed'
+
+To get the expected ouput in order, we must resolve an issue with callback
+*/
+
+function goToURL(callback){
+    setTimeout(() => {
+        console.log('You are on TechGlobal Website');
+        callback();
+    }, 2000);
+}
+
+function validateMainHeading(){
+    const actualText = '';  // You will learn how to get this later
+    const expectedText = 'Looking to pursue a new career?'; // Given in requirements
+    if(expectedText === actualText) console.log('Validation is successful!');
+    else console.log('Validation failed');
+}
+
+function test1(){
+    goToURL(() => {
+        validateMainHeading();
+    })
+}
+
+test1();
+// Using promises
+function goToURL(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.floor(Math.random() * 2)) resolve('URL is loaded!');
+            else reject('Website is broken');
+        }, 2000);
+    });
+}
+
+function validateMainHeading(){
+    const actualText = 'Looking to pursue a new career?';  // You will learn how to get this later
+    const expectedText = 'Looking to pursue a new career?'; // Given in requirements
+    if(expectedText === actualText) console.log('Validation is successful!');
+    else console.log('Validation failed');
+}
+
+function test1(){
+    goToURL().then(() => {
+        validateMainHeading();
+    }).catch((err) => {
+        console.log('ERROR:', err);
+    });
+    
+}
+
+test1();
